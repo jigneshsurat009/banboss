a91a7317feaea0a596ddee7b4a190ff5bb786e46
jigneshsurat009 <jigneshsurat009@gmail.com>
2025-05-25 14:51:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "28867109"))
+API_HASH = os.environ.get("API_HASH", "4324480d46d2b4a14daa7c93755703f2")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7968892213:AAFIEqpnynmHxQPwC1PeAK-zB9eGg_bjfbI")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://boychat960:lpFYrAe8B37VnMg5@cluster0.0u1ugsv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(os.environ.get("OWNER_ID", "944358553"))
